75eb1b6907e311bfdd71b1f97a558f1d306df0db
jorgejaimesg <158189835+Jorgejaimesg@users.noreply.github.com.>
2024-10-14 15:29:26 -0500

    fix: :bug: fix some warnings

--- backend/src/main/java/com/enterprisemanager/backend/domain/entities/PersonSupply.java
+++ backend/src/main/java/com/enterprisemanager/backend/domain/entities/PersonSupply.java
@@ -7,7 +7,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
